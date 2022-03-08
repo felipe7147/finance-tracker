e1cb3ebcfaba59b34ebe8cbd4ff4c7a865252e23
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2022-03-08 10:10:12 -0300

    Store secure API key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: 'Tpk_cec46bd0be284ce89b9815029949963f',
-                                  secret_token: 'Tsk_6ecfecbe89f349ce8962ebb7cab6d21a',
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                  secret_token: Rails.application.credentials.iex_client[:sandbox_api_secret_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
     client.price(ticker_symbol)
   end
