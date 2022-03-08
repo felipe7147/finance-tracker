c6ff565275caf35bf5328865841bc8a0a8b2e0f6
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2022-03-08 09:53:43 -0300

    Stock lookup: build class method to lookup stock info[

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: 'Tpk_cec46bd0be284ce89b9815029949963f',
+                                  secret_token: 'Tsk_6ecfecbe89f349ce8962ebb7cab6d21a',
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)
+  end
 end
