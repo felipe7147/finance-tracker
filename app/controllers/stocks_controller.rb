6691ad8528531235c25dea3d23214da6d39a1e60
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2022-03-09 14:33:27 -0300

    Setup JavaScript response

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,23 @@
-class StocksController < ApplicationController 
+class StocksController < ApplicationController
 
-  def search 
-    if params[:stock].present? 
-    @stock = Stock.new_lookup(params[:stock])
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
       if @stock
-      render 'users/my_portifolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portifolio_path
-      end
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result' }
+        end
+      end    
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portifolio_path
-   end
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result' }
+      end
+    end
   end
-
 end
