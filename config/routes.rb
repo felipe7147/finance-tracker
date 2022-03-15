3f4c83905e12ebdf26c2dbd2d58ecb21d73a7d3c
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2022-03-15 08:38:41 -0300

    friends list]

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portifolio', to: 'users#my_portifolio'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
   
 end
